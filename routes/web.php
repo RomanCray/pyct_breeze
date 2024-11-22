e7d6e7c2de3c75df3c3df23a3afc4ba56ec6bf34
RomanCray <santipau-1998@hotmail.com>
2024-11-22 16:12:29 -0500

    Second commit "Prueba"

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'bienvenido');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
